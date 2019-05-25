12a3ff1226ec0446299cc93b1cbc32fbeeabe33f
zhangwanyue <744404875@qq.com>
2019-05-25 23:26:47 +0800

    first commit

--- main/main.js
+++ main/main.js
@@ -1,4 +1,10 @@
-module.exports = function main() {
-    console.log("Debug Info");
+module.exports = function main(strNum) {
+    var lights = ['._.', '...', '._.', '._.', '...', '._.', '._.', '._.', '._.', '._.',
+        '|.|', '..|', '._|', '._|', '|_|', '|_.', '|_.', '..|', '|_|', '|_|',
+        '|_|', '..|', '|_.', '._|', '..|', '._|', '|_|', '..|', '|_|', '..|]'];
+    for(let i = 0; i < strNum.length; i++){
+        console.log(lights);
+    }
+
     return 'Hello World!';
 };
