76f18cffb1d0caeb128e7d76ef619ec68994b5ee
zhangwanyue <744404875@qq.com>
2019-05-26 00:10:49 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -1,10 +1,16 @@
 module.exports = function main(strNum) {
     var lights = ['._.', '...', '._.', '._.', '...', '._.', '._.', '._.', '._.', '._.',
         '|.|', '..|', '._|', '._|', '|_|', '|_.', '|_.', '..|', '|_|', '|_|',
-        '|_|', '..|', '|_.', '._|', '..|', '._|', '|_|', '..|', '|_|', '..|]'];
-    for(let i = 0; i < strNum.length; i++){
-        console.log(lights);
+        '|_|', '..|', '|_.', '._|', '..|', '._|', '|_|', '..|', '|_|', '..|'];
+    var result = '';
+    for(let i = 0; (i+10) <= lights.length; i += 10){
+        for(let j = 0; j < strNum.length; j++){
+            result += lights[parseInt(strNum.charAt(j)) + i];
+            if(j < strNum.length-1){
+                result += ' ';
+            }
+        }
+        result += '\n';
     }
-
-    return 'Hello World!';
+    return result;
 };
